0e90f5472f668a3f51efd9ba501c05ef93d7d87f
antonio <xang813@gmail.com>
2024-02-19 14:26:40 -0500

    segunda funcion

--- Script_AE.sh
+++ Script_AE.sh
@@ -15,6 +15,15 @@ echo "ping to another host"
 read -p "enter the ip address of the host :" remote_host
 ping -p $remote_host
 
+#Second Feature
+
+echo " Display help for windows"
+
+help 
+
+
+
+
 
 
 
